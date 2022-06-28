72b5a04fde40bfd8598882943806ea6198d48842
Oussama Souayri <souayri.oussama@gmail.com>
2022-06-28 18:09:56 +0100

    router & nested routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {AboutComponent} from './about/about.component';
+import {PostsComponent} from './posts/posts.component';
+import {CreateComponent} from './posts/create/create.component';
+import {ShowComponent} from './posts/show/show.component';
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'posts',
+    children:[
+      {
+        path:'',
+        component:PostsComponent
+      },
+      {
+        path:'create',
+        component:CreateComponent
+      },
+      {
+        path:'show',
+        component:ShowComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
